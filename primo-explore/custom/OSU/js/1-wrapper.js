a8fbeafbe766c34798f922970841cc065a73e761
Dan M <dan.moore@oregonstate.edu>
2018-02-20 15:13:00 -0800

    fixed hide/show summit libraries button

--- primo-explore/custom/OSU/js/1-wrapper.js
+++ primo-explore/custom/OSU/js/1-wrapper.js
@@ -8,5 +8,3 @@ var app = angular.module('centralCustom', ['angularLoad']);
 var applocal = angular.module('viewCustom', ['angularLoad']);
 
 /************************************* END Bootstrap Script ************************************/
-
-console.log("from central package");
