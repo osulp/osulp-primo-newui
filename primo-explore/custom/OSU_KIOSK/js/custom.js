13239ca59cb4aac81771a4f7ee511c5707311be3
Dan M <dan.moore@oregonstate.edu>
2018-03-28 15:10:45 -0700

    fixed minor JS formatting error

--- primo-explore/custom/OSU_KIOSK/js/custom.js
+++ primo-explore/custom/OSU_KIOSK/js/custom.js
@@ -3,19 +3,15 @@
     'use strict';
     
     /************************************* BEGIN Bootstrap Script ************************************/
-    
-    var app = angular.module('centralCustom', ['angularLoad']);
-    
-    var applocal = angular.module('viewCustom', ['angularLoad']);
-    
+    var app = angular.module('viewCustom', ['angularLoad']);
     /************************************* END Bootstrap Script ************************************/
        
     /** Show search scopes by default on basic searches **/
-        applocal.component('prmSearchBarAfter', {
+        app.component('prmSearchBarAfter', {
             bindings: { parentCtrl: '<' },
             controller: 'SearchBarAfterController'
         });
-        applocal.controller('SearchBarAfterController', ['angularLoad', function (angularLoad) {
+        app.controller('SearchBarAfterController', ['angularLoad', function (angularLoad) {
             var vm = this;
             vm.parentCtrl.showTabsAndScopes = true;
         }]);
@@ -30,10 +26,7 @@
         ga('create', 'UA-35760875-20');
         ga('send', 'pageview');
         ga('set', 'anonymizeIp', true);
-    
 
-
-    
     /* Loads jQuery into Primo application */
     app.component('prmTopBarBefore', {
         bindings: { parentCtrl: '<' },
@@ -44,7 +37,5 @@
         },
         template: '<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/themes/ui-lightness/jquery-ui.css" rel="stylesheet"><link href="https://cdnjs.cloudflare.com/ajax/libs/virtual-keyboard/1.28.0/css/keyboard.min.css" rel="stylesheet">'
     });
-
-    /* End anonymous function */     
     })();
     
